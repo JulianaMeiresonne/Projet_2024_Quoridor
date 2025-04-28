ce4a34e2c1736cb648b5aca042ab3591ed20e9cb
Juliana <23342ecam@gmail.com>
2025-04-28 11:06:57 +0200

    put generated_pieces and input_move outside to be test

--- player.py
+++ player.py
@@ -6,6 +6,44 @@ import random
 port_perso = 8888
 port_serveur_global = 3000 # connection au serveur du prof
 IP_serveur_global = "localhost" # adress IP du serveur du prof
+chosen_pieces =[]
+all_position = []
+
+
+def generated_pieces(): # génèrer la liste de tous les moves
+          # Big/Small: B/S
+          # Dark/Light: D/L
+          # Empty/Full: E/F
+          # Cylinder/Prism: C/P
+          for size in ["B", "S"]:
+              for color in ["D", "L"]:
+                  for weight in ["E", "F"]:
+                      for shape in ["C", "P"]:
+                          chosen_pieces.append(f"{size}{color}{weight}{shape}")
+
+
+def input_move(game_board,piece): #choisit les move de manière random
+              # Move
+          # {
+          #   "pos": <index>,        # between 0 and 15 or null if first move
+          #   "piece": <piece_str>,  # piece for the opponent example "BDEC"
+          # }
+              all_position =list(range(len(game_board))) #range renvoie une object range donc il faut transformer en liste
+              index = 0
+              for n in game_board:
+                  if n != None:
+                      chosen_pieces.remove(n)
+                      if index in all_position: 
+                          all_position.remove(index)
+                  index +=1
+              pos = random.choice(all_position)
+              if piece in chosen_pieces: #pour éviter les bad move quand on choisit la piece
+                chosen_pieces.remove(piece)
+              piece = random.choice(chosen_pieces)
+              if len(piece) == 0:
+                  piece = None
+              return {"pos": pos, "piece": piece}
+
 
 #Inscription au serveur
 s_inscription = socket.socket() #TCP
@@ -19,7 +57,6 @@ message_connection = {
 s_inscription.send(json.dumps(message_connection).encode()) #!!!!!! json.dumps().encode(), il faut pas oublié encode()
 message = s_inscription.recv(1024)
 message_receive =json.loads(message.decode())
-print(message)
 
 
 #Communication serveur (envoyer et recevoir information)
@@ -34,50 +71,10 @@ while message_receive["response"] == "ok":
     if message_receive_ping["request"] == "ping":
       message_pong = {"response": "pong"}
       client.send(json.dumps(message_pong).encode())
-    elif message_receive_ping["request"] == "play":
-      chosen_pieces =[]
-      all_position =list(range(len(message_receive_ping["state"]["board"]))) #range renvoie une object range donc il faut transformer en liste
-      def generated_pieces(): # génèrer la liste de tous les moves
-          # Big/Small: B/S
-          # Dark/Light: D/L
-          # Empty/Full: E/F
-          # Cylinder/Prism: C/P
-          for size in ["B", "S"]:
-              for color in ["D", "L"]:
-                  for weight in ["E", "F"]:
-                      for shape in ["C", "P"]:
-                          chosen_pieces.append(f"{size}{color}{weight}{shape}") 
-
-          # Move
-          # {
-          #   "pos": <index>,        # between 0 and 15 or null if first move
-          #   "piece": <piece_str>,  # piece for the opponent example "BDEC"
-          # }
-      def input_move(): #choisit les move de manière random
-              index = 0
-              for n in message_receive_ping["state"]["board"]:
-                  if n != None:
-                      chosen_pieces.remove(n)
-                      if index in all_position: 
-                          all_position.remove(index)
-                  index +=1
-              pos = random.choice(all_position)
-              if message_receive_ping["state"]["piece"] in chosen_pieces: #pour éviter les bad move quand on choisit la piece
-                chosen_pieces.remove(message_receive_ping["state"]["piece"])
-
-                #This is a Bad Move. Piece 'SLFP' not available => car existe pas ou dejà choisit ???
-              piece = random.choice(chosen_pieces)
-              if len(piece) == 0:
-                  piece = None
-              return {"pos": pos, "piece": piece}
-      
+    elif message_receive_ping["request"] == "play": 
       generated_pieces()
-      move = input_move()
-      move_sent = {
-                                "response": "move",
-                                "move": move,
-                                "message": "Fun message"
-                              }
+      move = input_move(message_receive_ping["state"]["board"],message_receive_ping["state"]["piece"])
+      move_sent = { "response": "move","move": move,"message": "Fun message"}
       client.send(json.dumps(move_sent).encode())
 
 # python3 server.py quarto
