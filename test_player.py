e065a7bba6998e0733aacbc14354c46ee67a5751
Saadi <23268@ecam.be>
2025-04-29 19:22:43 +0200

    add test of position

--- test_player.py
+++ test_player.py
@@ -1,3 +1,17 @@
 import pytest
 import player
 
+state={"board": {                                                          #on définit un plateau avec certaine pièce placée
+            (0, 0): "piece1",(0, 1): None,(0,2):None,(0,3):None,
+            (1, 1): None, (1, 1): None,(1,2):None,(1,3):None,
+            (2, 0): "piece2",(2, 1): None,(2,2):None,(2,3):None,
+            (3, 0): "piece3",(3, 1): None,(3,2):None,(3,3):None,
+            }
+        }
+pos=(0,0)                                                                   #on prends une position ayant déjà une pièce pour tester l'erreur
+def test_next():
+    with pytest.raises(ValueError):
+        if "pos" not in [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]:
+            raise ValueError
+        if state["board"][pos] is not None:
+            raise ValueError
