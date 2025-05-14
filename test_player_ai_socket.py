cfe18c7178643995eba785539470069e635f4b48
Saadi <23268@ecam.be>
2025-05-14 19:11:43 +0200

    Add a test to ensure we cannot play a piece that has already been chosen

--- test_player_ai_socket.py
+++ test_player_ai_socket.py
@@ -15,3 +15,19 @@ def test_position():
             raise ValueError
         if state["board"][pos] is not None:
             raise ValueError
+
+def test_moves():
+    # Définir un plateau de jeu avec certaines pièces placées
+    game_board={(0, 0): "BDEC",(0, 1): None,(0,2):None,(0,3):None,
+            (1, 1): None, (1, 1): None,(1,2):None,(1,3):None,
+            (2, 0): "BDEP",(2, 1): None,(2,2):None,(2,3):None,
+            (3, 0): "BLEC",(3, 1): None,(3,2):None,(3,3):None
+                }
+    piece = "BSCP"  # La pièce que vous souhaitez tester
+    result = player_ai_socket.moves(game_board, piece)
+    for move in result.values():
+        assert move["piece"] != piece
+
+
+ 
+
